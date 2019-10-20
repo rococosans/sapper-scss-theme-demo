6c66205e91c67b8a9a8f5869373e29550a1a5203
rococosans <dennyscakeslam@gmail.com>
2019-10-19 21:40:44 -0700

    initial

---
+++ src/routes/_layout.svelte
@@ -0,0 +1,25 @@
+<script>
+  import Nav from "../components/Nav.svelte";
+
+  export let segment;
+</script>
+
+<style lang="scss" global>
+  @import "./styles/global.scss";
+  @import "./styles/theme.scss";
+
+  main {
+    position: relative;
+    max-width: $width;
+    background-color: white;
+    padding: 2em;
+    margin: 0 auto;
+    box-sizing: border-box;
+  }
+</style>
+
+<Nav {segment}></Nav>
+
+<main>
+  <slot></slot>
+</main>
